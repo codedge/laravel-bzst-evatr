a83a44a463d57db16b807153f578da1c28a9abc1
Holger Lösken <holger.loesken@codedge.de>
2016-06-20 18:59:58 +0200

    Adding basic unit tests

---
+++ tests/AbstractEvatrTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Codedge\Evatr\Tests;
+
+use Codedge\Evatr\AbstractEvatr;
+use Orchestra\Testbench\TestCase;
+
+/**
+ * AbstractEvatrTest.
+ *
+ * @author Holger Lösken <holger.loesken@codedge.de>
+ * @copyright See LICENSE file that was distributed with this source code.
+ *
+ */
+class AbstractEvatrTest extends TestCase
+{
+    public function testGetPrintConfirmationOption()
+    {
+        /** @var AbstractEvatr $mock */
+        $mock = $this->getMockForAbstractClass('Codedge\Evatr\AbstractEvatr');
+
+        $this->assertSame(true, $mock->_getPrintConfirmationOption('ja'));
+        $this->assertSame(false, $mock->_getPrintConfirmationOption('nein'));
+    }
+
+    public function testSetPrintConfirmationOption()
+    {
+        /** @var AbstractEvatr $mock */
+        $mock = $this->getMockForAbstractClass('Codedge\Evatr\AbstractEvatr');
+
+        $this->assertSame('ja', $mock->_setPrintConfirmationOption(true));
+        $this->assertSame('nein', $mock->_setPrintConfirmationOption(false));
+    }
+}
